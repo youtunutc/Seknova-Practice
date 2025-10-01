cfa2c1d1aea43337388d5440f8fa12a5c120f88d
youtunutc <youtunutc@gmail.com>
2025-10-01 16:35:37 +0800

    登入畫面-跳轉註冊畫面

---
+++ Seknova-Practice/Controller/SignUp/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  Seknova-Practice
+//
+//  Created by imac-2627 on 2025/10/1.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

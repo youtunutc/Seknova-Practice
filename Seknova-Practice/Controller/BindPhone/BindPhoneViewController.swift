d369d7d4128fe443a68f2b7361d1625fad336150
youtunutc <youtunutc@gmail.com>
2025-10-07 10:39:43 +0800

    重新驗證信頁面-按下一步跳轉到登入頁面自動登入

---
+++ Seknova-Practice/Controller/BindPhone/BindPhoneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BindPhoneViewController.swift
+//  Seknova-Practice
+//
+//  Created by imac-2627 on 2025/10/7.
+//
+
+import UIKit
+
+class BindPhoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
